ee417240a46b567673222fb94fd8d2c59d33a068
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-04-05 23:56:45 -0300

    classe com atributos e metodos

---
+++ src/br/com/thiago/javapoo/classes/Veiculo.java
@@ -0,0 +1,21 @@
+package br.com.thiago.javapoo.classes;
+
+public class Veiculo {
+
+	public String nome;
+	public String marca;
+	public String chassi;
+	public Integer quantidadeRodas;
+	
+	public void ligar() {
+		
+	}
+	
+	public void desligar() {
+		
+	}
+	
+	public float abastecer(float litros) {
+		return litros;
+	}
+}
