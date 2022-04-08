8389ca019361297d567277e04e204464f5b8031e
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-04-08 10:22:47 -0300

    encapsulamento

--- src/br/com/thiago/javapoo/classes/Veiculo.java
+++ src/br/com/thiago/javapoo/classes/Veiculo.java
@@ -2,11 +2,53 @@ package br.com.thiago.javapoo.classes;
 
 public class Veiculo {
 
-	public String nome;
-	public String marca;
-	public String chassi;
-	public Integer quantidadeRodas;
+	private String nome;
+	private String marca;
+	private String chassi;
+	private Integer quantidadeRodas;
+	private float quantidadeCombustivel;
 	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(String chassi) throws Exception {
+		if(chassi.length() == 5) {
+			this.chassi = chassi;	
+		}else {
+			throw new Exception("Chassi inválido");
+		}
+		
+	}
+
+	public Integer getQuantidadeRodas() {
+		return quantidadeRodas;
+	}
+
+	public void setQuantidadeRodas(Integer quantidadeRodas) {
+		this.quantidadeRodas = quantidadeRodas;
+	}
+
+	public float getQuantidadeCombustivel() {
+		return quantidadeCombustivel;
+	}
+
 	public void ligar() {
 		System.out.println("O veiculo ligou");
 	}
@@ -15,7 +57,7 @@ public class Veiculo {
 		System.out.println("O veiculo desligou");
 	}
 	
-	public float abastecer(float litros) {
-		return litros;
+	public void abastecer(float litros) {
+		quantidadeCombustivel += litros;
 	}
 }
