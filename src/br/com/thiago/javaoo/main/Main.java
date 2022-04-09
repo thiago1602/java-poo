43c958083198c4042e9e07e8ed340b80b06f1dc1
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-04-08 21:07:28 -0300

    sobrecargas e assinaturas de construtores

--- src/br/com/thiago/javaoo/main/Main.java
+++ src/br/com/thiago/javaoo/main/Main.java
@@ -7,15 +7,18 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-			Carro corsa = new Carro();
-			corsa.setMarca("GM");
-			corsa.setNome("Corsa");
+//			Carro corsa = new Carro();
+//			corsa.setMarca("GM");
+//			corsa.setNome("Corsa");
+//			
+			Carro corsa = new Carro("Corsa", "GM");
 			corsa.setChassi("ABCDEF");
 			corsa.abastecer(10);
 			System.out.println(corsa.getNome());
 			System.out.println(corsa.getQuantidadeCombustivel());
-			System.out.println(corsa.getQuantidadeRodas()());
+			System.out.println(corsa.getQuantidadeRodas());
 			corsa.ligar();
+			Carro celta = new Carro();
 		} catch (Exception e) {
 			System.out.println("Ocorreu um erro: " + e.getMessage());
 		}
--- src/br/com/thiago/javapoo/classes/Carro.java
+++ src/br/com/thiago/javapoo/classes/Carro.java
@@ -6,4 +6,17 @@ public class Carro extends Veiculo {
 	public Carro() {
 		this.quantidadeRodas = 4;
 	}
+	
+	public Carro(String nome, String marca) {
+		this.quantidadeRodas = 4;
+		this.setNome(nome);
+		this.setMarca(marca);
+	}
+	public Carro(String nome, float combustivel) {
+		
+	}
+	
+	public Carro(float combustivel, String nome) {
+		
+	}
 }
